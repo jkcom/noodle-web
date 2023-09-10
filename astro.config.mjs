bcbdb04558954238845df462113bc2abc8fef378
Josef Kjaergaard <josef@josefkjaergaard.com>
2023-09-10 13:48:48 +0200

    mw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercelEdge from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 import compressor from "astro-compressor";
 
@@ -8,5 +8,7 @@ import compressor from "astro-compressor";
 export default defineConfig({
   output: "server",
   integrations: [react(), compressor()],
-  adapter: vercelEdge(),
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
 });
